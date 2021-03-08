3a31dd82ce725b42c4658b158b350fe94646e337
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-08 19:33:53 +0300

    Create totalAmountOfPointsBest.js

---
+++ Mar2021/totalAmountOfPointsBest.js
@@ -0,0 +1,18 @@
+// I have to remember that strings of numbers can be called on as numbers and are implicitly coerced to a number like below.
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+//   I should have realized that I only needed two if statement since the last situation nothing is added to the
+function points(games) {
+    let total = 0;
+    games.map(game => {
+      if (game[0] === game[2]) {
+        total += 1;
+      } else if (game[0] > game[2]) {
+        total += 3;
+      }
+    });
+    return total;
+  }

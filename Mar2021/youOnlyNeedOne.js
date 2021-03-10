d00c87f67e3c73eef6ded5aa98b4b5417a2abdd5
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-10 19:52:05 +0300

    Create youOnlyNeedOne.js

---
+++ Mar2021/youOnlyNeedOne.js
@@ -0,0 +1,9 @@
+function check(a, x) {
+    return a.some(el => el === x);
+  }
+console.log(
+    check([66, 101], 66),// true);
+    check([80, 117, 115, 104, 45, 85, 112, 115], 45),// true);
+    check(['t', 'e', 's', 't'], 'e'),// true);
+    check(['what', 'a', 'great', 'kata'], 'kat'),// false);
+)

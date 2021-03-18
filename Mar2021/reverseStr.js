20bb164533e8f0b3c59be857e2bb526829167d42
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-18 22:04:55 +0300

    Create reverseStr.js

---
+++ Mar2021/reverseStr.js
@@ -0,0 +1,12 @@
+function solution(str){
+
+
+
+        return str.split("").reverse().join("");
+}
+console.log(
+    solution('world'),// 'dlrow')
+    solution('hello'),// 'olleh')
+    solution(''),// '')
+    solution('h'),// 'h')
+)

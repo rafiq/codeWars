446ffc09e3ce3f7626675141aed2def6397a40f4
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-20 09:41:24 +0300

    Create sumOfDigitsDigitalRootBest.js

---
+++ Mar2021/sumOfDigitsDigitalRootBest.js
@@ -0,0 +1,8 @@
+// Here the recursive method is working great.
+
+function digital_root(n) {
+    if (n < 10) return n;
+
+    return digital_root(
+      n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+  }

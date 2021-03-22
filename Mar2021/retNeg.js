295fda86755f74245cef380bf4d7035ad19ff42a
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-22 16:23:22 +0300

    Create retNeg.js

---
+++ Mar2021/retNeg.js
@@ -0,0 +1,9 @@
+function makeNegative(num) {
+    return Math.abs(num) * -1;
+  }
+console.log(
+makeNegative(1), // return -1
+makeNegative(-5), // return -5
+makeNegative(0), // return 0
+makeNegative(0.12), // return -0.12
+)

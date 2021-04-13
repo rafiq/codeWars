5895ee5188cb1c60ff795a8f8cc062bb48dd947d
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-04-13 21:03:03 +0300

    Create mumbling.js

---
+++ Mar2021/mumbling.js
@@ -0,0 +1,12 @@
+function accum(s) {
+    return s.split("").map((letter,idx) => {
+        return letter.toUpperCase() + letter.toLowerCase().repeat(idx)
+    }).join("-");
+}
+console.log(
+    accum("ZpglnRxqenU"),//"Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+	accum("NyffsGeyylB"),//"N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+	accum("MjtkuBovqrU"),//"M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+	accum("EvidjUnokmM"),//"E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+	accum("HbideVbxncC"),//"H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+)

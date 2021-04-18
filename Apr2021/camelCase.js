8a35b94031d5728a8319736b1d5c9299ed11bdf0
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-04-18 19:34:10 +0300

    Create camelCase.js

---
+++ Apr2021/camelCase.js
@@ -0,0 +1,10 @@
+String.prototype.camelCase=function(){
+
+
+  console.log(
+   "test case".camelCase(),// "TestCase");
+   "camel case method".camelCase(),// "CamelCaseMethod");
+   "say hello ".camelCase(),// "SayHello");
+   " camel case word".camelCase(),// "CamelCaseWord");
+   "".camelCase(),// "");
+  )

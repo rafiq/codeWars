fe28318d93cd11933abcfde2ee034e1b89eb40f1
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-04-19 20:02:58 +0300

    Create isRubyComing.js

---
+++ Mar2021/isRubyComing.js
@@ -0,0 +1,20 @@
+var list1 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+  ];
+
+  var list2 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' }
+  ];
+
+function isRubyComing(list) {
+
+
+    return list.find(el => el.language === "Ruby") ? true : false;
+  }
+  console.log(
+    isRubyComing(list1), //true);
+    isRubyComing(list2), //false);
+  )

1140db6642664865ad26e71dd97c01a59a165a0e
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-04-24 21:48:34 +0300

    Create formatWith.js

---
+++ Mar2021/formatWith.js
@@ -0,0 +1,10 @@
+String.prototype.formatWith = function () {
+    return
+    }
+console.log(
+    'Hello {0}'.formatWith('Codewars'), //'Hello Codewars');
+    '{0} is a {1} {2}'.formatWith('Today','good','day'), //'Today is a good day');
+    '{0} + {0} = {1}'.formatWith('1','2'), //'1 + 1 = 2');
+    'Hello {0}. This {1} is bring a {2} to work day'.formatWith('World','Monday','dog'), //'Hello World. This Monday is bring a dog to work day');
+    'Testing {1}'.formatWith('kata'),//'Testing {1}');
+)

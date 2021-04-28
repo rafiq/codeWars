bee38f6cb2809b225bd95ab1d3d081b7e83a5376
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-04-28 21:20:10 +0300

    Create ES6StringAdd.js

---
+++ Mar2021/ES6StringAdd.js
@@ -0,0 +1,9 @@
+function joinStrings(string1, string2){
+    return `${string1} ${string2}`;
+ }
+
+ console.log(
+    joinStrings('string1', 'string2'),// 'string1 string2');
+    joinStrings('testing', 'testing'),// 'testing testing');
+    joinStrings(134, 234),// '134 234')//;
+ )

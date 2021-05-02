bb6e49eeb498aea24bdb7b65c33addc5b437ebc1
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-02 21:50:35 +0300

    Create distanceBetweenTwoPoints.js

---
+++ Mar2021/distanceBetweenTwoPoints.js
@@ -0,0 +1,11 @@
+function distanceBetweenPoints(a, b) {
+
+
+
+    return a - b;
+  }
+console.log(
+    distanceBetweenPoints(new Point(3, 3), new Point(3, 3)),// 0);
+    distanceBetweenPoints(new Point(1, 6), new Point(4, 2)),// 5);
+    distanceBetweenPoints(new Point(-10.2, 12.5), new Point(0.3, 14.7)).toFixed(6), //10.728001);
+)

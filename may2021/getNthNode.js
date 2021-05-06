015e8e0b47e314c1f5db919577a38b8509a320a2
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-06 21:29:17 +0300

    Create getNthNode.js

---
+++ may2021/getNthNode.js
@@ -0,0 +1,17 @@
+function Node(data) {
+    this.data = data;
+    this.next = null;
+  }
+
+  function getNth(node, index) {
+
+    return node;
+  }
+
+  let a = new Node();
+  let b = new Node();
+a.data = 5;
+b.data = 7
+  a.next = b;
+
+  console.log(getNth(a))

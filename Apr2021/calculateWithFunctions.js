c74496fe2c21f741cdf7e9ae0723124d6ef4c09a
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-14 22:49:32 +0300

    Create calculateWithFunctions.js

---
+++ Apr2021/calculateWithFunctions.js
@@ -0,0 +1,38 @@
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {
+    return 5
+}
+function six() {}
+function seven() {
+    return 7
+}
+function eight() {}
+function nine() {}
+
+function plus(num) {
+
+}
+function minus() {}
+function times() {
+    return "*"
+}
+function dividedBy() {}
+ console.log(
+    seven(times(five())),// 35);
+    // four(plus(nine())),// 13);
+    // eight(minus(three())),// 5);
+    // six(dividedBy(two())),// 3);
+ )
+
+ 'zero one two three four five six seven eight nine'.split(' ').forEach(
+    (mth, num) => this[mth] = (f = val => val) => f(num)
+  )
+
+  let plus      = (r) => (l) => l + r
+  let minus     = (r) => (l) => l - r
+  let times     = (r) => (l) => l * r
+  let dividedBy = (r) => (l) => l / r

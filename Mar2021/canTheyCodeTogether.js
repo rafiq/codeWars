499c1914737bb2b888efce7c331dc39ee6b36a46
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-16 19:49:23 +0300

    Create canTheyCodeTogether.js

---
+++ Mar2021/canTheyCodeTogether.js
@@ -0,0 +1,20 @@
+var list1 = [
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+  ];
+
+  var list2 = [
+    { firstName: 'Mariami', lastName: 'G.', country: 'Georgia', continent: 'Europe', age: 29, language: 'Python' },
+    { firstName: 'Mia', lastName: 'H.', country: 'Germany', continent: 'Europe', age: 39, language: 'Ruby' },
+    { firstName: 'Maria', lastName: 'I.', country: 'Greece', continent: 'Europe', age: 32, language: 'C' },
+  ];
+
+function isSameLanguage(list) {
+
+    return list.map(el => el.language).every((el,idx,arr) => el === arr[0]);
+  }
+  console.log(
+    isSameLanguage(list1),//true);
+    isSameLanguage(list2),//false);
+  )

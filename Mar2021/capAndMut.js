38b7dc20144fefcf596e214d211861323124c4f5
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-16 19:48:58 +0300

    Create capAndMut.js

---
+++ Mar2021/capAndMut.js
@@ -0,0 +1,9 @@
+function capitalizeWord(word) {
+    return word[0].toUpperCase() + word.slice(1);
+
+  }
+  console.log(
+    capitalizeWord('word'),// 'Word');
+    capitalizeWord('i'), //'I');
+    capitalizeWord('glasswear'), //'Glasswear');
+  )

74548a34a522588da114189e6112aff74af0f59e
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-28 18:06:04 +0300

    Create closestNumToTen.js

---
+++ Apr2021/closestNumToTen.js
@@ -0,0 +1,16 @@
+const closestMultiple10 = num => {
+    return Math.round(num / 10) * 10;
+  };
+  console.log(
+    closestMultiple10(54),// 50);
+
+// it('should return 60 for num = 55', function() {
+  closestMultiple10(55),// 60);
+  )
+
+[
+[a	b	c	d	e	1	2	3]
+[f	g	h	i	j	4	5	6]
+[k	l	m	n	o	7	8	9]
+[p	q	r	s	t	.	@	0]
+[u	v	w	x	y	z	_	/]]

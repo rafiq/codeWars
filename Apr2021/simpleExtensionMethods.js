3470e57001d7c186000c7833f0bfc20c16d43bf7
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-06-12 20:47:37 +0300

    Create simpleExtensionMethods.js

---
+++ Apr2021/simpleExtensionMethods.js
@@ -0,0 +1,28 @@
+static class SimpleExtensions {
+
+    let name = () => {
+
+    }
+
+    sayHello() {
+        return `Hello, ${this.name}`
+    }
+    // SayGoodbye...
+
+
+  }
+  console.log(
+      name.sayHello(),//"Hello, Alex!"
+    // namespace Solution {
+    //     using NUnit.Framework;
+    //     using System;
+
+    //     [TestFixture]
+    //     public class Tests
+    //     {
+    //       [Test]
+    //       public void ExampleTests() {
+    //         const string name = "Alex";
+    //         Assert.AreEqual("Hello, Alex!", name.SayHello());
+    //         Assert.AreEqual("Goodbye, Alex. See you again soon!", name.SayGoodbye());
+  )

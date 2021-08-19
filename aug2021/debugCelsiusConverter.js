d0a77c8e86d8d2a78f4079ea5c0a02de0941f28d
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-08-18 20:27:02 -0700

    Create debugCelsiusConverter.js

---
+++ aug2021/debugCelsiusConverter.js
@@ -0,0 +1,17 @@
+function weatherInfo (temp) {
+    let c = convertToCelsius(temp)
+
+    if (c < 0) return (c + " is freezing temperature")
+    else return (c + " is above freezing temperature")
+  }
+
+  function convertToCelsius (temperature) {
+    var celsius = (temperature - 32) * (5/9)
+    return celsius
+  }
+  console.log(
+
+        weatherInfo(50),// '10 is above freezing temperature')
+        weatherInfo(23),//  '-5 is freezing temperature')
+
+  )

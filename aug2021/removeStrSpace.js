738b2fa24e3709406fbc188a4477a6fa1748e131
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-08-20 08:05:46 -0700

    Create removeStrSpace.js

---
+++ aug2021/removeStrSpace.js
@@ -0,0 +1,13 @@
+function noSpace(x){
+    return x.split("").reduce((acc,curr) => {
+        if (curr !== " ") acc += curr
+         return acc
+    },"")
+}
+console.log(
+    noSpace(
+        noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'),// '8j8mBliB8gimjB8B8jlB');
+        noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'),// '88Bifk8hB8BB8BBBB888chl8BhBfd');
+        noSpace('8aaaaa dddd r     '),// '8aaaaaddddr');
+    )
+)

afe17625959e01a68a7ea231309f73a0040491dc
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-08-24 21:18:24 -0700

    Create repeatReverse.sql

---
+++ aug2021/repeatReverse.sql
@@ -0,0 +1,7 @@
+SELECT
+    REPEAT(name,3) name, REVERSE(characteristics) characteristics
+FROM
+    monsters;
+
+
+    SELECT REPEAT(name, 3) AS name, REVERSE(characteristics) AS characteristics FROM monsters
